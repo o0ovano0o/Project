22debac1006871730cc8ad3200eeb547d7aaec4b
dominan <minhanh.th99@gmail.com>
2021-04-13 09:58:02 +0700

    style listticket

--- frontend/GuardScreens/ListTicket_AddNew.tsx
+++ frontend/GuardScreens/ListTicket_AddNew.tsx
@@ -4,6 +4,7 @@ import { AntDesign,Feather,FontAwesome ,MaterialCommunityIcons,Ionicons,Fontisto
 import { TabView, SceneMap } from 'react-native-tab-view';
 import Modal from "react-native-modal";
 import { SearchBar } from 'react-native-elements';
+import styles from '../Style/ListTicketAddNew';
 function Item(){
     const [visiable, setCount] = useState(0);
     return(
@@ -281,79 +282,5 @@ function ListTicket_AddNew({ navigation: { navigate } }) {
 }
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white"   
-  },
-  tabback:{
-      height: 50,
-      width:width,
-      backgroundColor: "#16f198"   ,
-      justifyContent:'center',
-      alignItems:'center',
-      borderBottomWidth:1,
-      borderBottomColor:"#CCCCCC",
-      flexDirection:'row'
-  },
-  searchstyle:{
-    height:45, 
-    borderRadius:10,
-    backgroundColor:'white', 
-    borderColor:'white', 
-    width:width-20
-  },
-  profile:{
-      height: height-50,
-      width: width,     
-  },
-  image:{
-    height:70,
-    width:70,
-    borderRadius:70,
-    marginLeft:20,   
-  },
-  item:{
-    height:80,
-    borderColor:"#CCCCCC", 
-    borderWidth:1, 
-    borderRadius:20, 
-    marginHorizontal:10, 
-    marginBottom:10,
-    width:160,
-    flex:1
-  },
-  itemimage:{
-    flex:2, 
-    justifyContent:'center', 
-    alignItems:'center',
-  },
-  namecar:{
-    fontWeight:'bold', 
-    fontSize:14, 
-    marginTop:10, 
-    marginLeft:10
-  },
-  textcar:{
-    fontWeight:'normal', 
-    fontSize:12, 
-    marginTop:5,
-    marginLeft:10
-  },
-  icondelete:{
-    position:'absolute', 
-    right:10, 
-    top:10
-  },
-  circle:{
-    height:50, 
-    width:50, 
-    
-    justifyContent:'center', 
-    alignItems:'center' 
-    
-  }
-
-});
 
 export default ListTicket_AddNew;
